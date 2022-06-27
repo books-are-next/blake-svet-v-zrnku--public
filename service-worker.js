b6331a42d55657a7b047bc4b93c88bf613108621
gabGodiskova <gabgodiskova@gmail.com>
2022-06-27 14:28:15 +0000

    Deploying to docs from @ books-are-next/blake-svet-v-zrnku-pisku@a4a41346dd23825d4c0a3cca6429515acfa7e4d2 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-9f1ca01';
+const CACHE = 'cache-a4a4134';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
